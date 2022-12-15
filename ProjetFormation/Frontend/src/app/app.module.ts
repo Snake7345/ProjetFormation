681835885981a2afd6c317eaa5328dbbbbfd6fb4
Snake7345 <thebasketman7@hotmail.com>
2022-12-15 10:42:07 +0100

    Ajout des components et mise a jour des packages
    Ajout de components et mise a jour des packages :
    - Ajout de footer
    - Ajout de navbar
    - Ajout de la page 404

--- ProjetFormation/Frontend/src/app/app.module.ts
+++ ProjetFormation/Frontend/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/compos/navbar/navbar.component';
+import { FourofourComponent } from './shared/compos/fourofour/fourofour.component';
+import { FooterComponent } from './shared/compos/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FourofourComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
