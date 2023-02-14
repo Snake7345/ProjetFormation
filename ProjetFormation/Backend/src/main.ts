fa7821a1fe2d632ce552575e4a64fc5b7a1953e1
Snake7345 <thebasketman7@hotmail.com>
2023-02-14 13:15:25 +0100

    petite correction

--- ProjetFormation/Backend/src/main.ts
+++ ProjetFormation/Backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const swaggerConfig = new DocumentBuilder()
     .setTitle('ProjetFormation')
     .setDescription('API fournissant les informations sur ProjetFormation NestJS API')
-    .setVersion('1.0')
+    .setVersion('0.8.0beta')
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
