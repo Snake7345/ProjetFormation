afe4f3515d4f1a05470d8935c19a854ed105d5b0
Snake7345 <thebasketman7@hotmail.com>
2023-02-14 11:16:24 +0100

    v0.8.0 - Démarrage projet perso
    - Ajout des composants
    - Ajout de swagger dans le programme qui permet de verifier tous les chemins et voir si ils sont valides : taper localhost le port et ensuite /api# pour afficher swagger

--- ProjetFormation/Backend/src/main.ts
+++ ProjetFormation/Backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { SERVER_PORT } from './config/constants';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 /*Port sur lequel s'exécute le programme et permet de ne pas le mettre en dur dans le fichier
  * Se base sur la variable server_port du fichier constants, cette variable est contenu dans le .env
@@ -10,6 +11,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.enableCors();
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('ProjetFormation')
+    .setDescription('API fournissant les informations sur ProjetFormation NestJS API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   //Server port
   const port = +configService.get<number>(SERVER_PORT) || 3000;
   await app.listen(port);
