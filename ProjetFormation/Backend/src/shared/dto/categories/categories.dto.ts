b23ad73475bc94e44766c8c6c614539be1ec02b3
Snake7345 <thebasketman7@hotmail.com>
2023-02-22 16:15:40 +0100

    V0.8.6.7
    - Les erreurs commencent à être mis en place dans les dtos
    - Les énumérations sont mise en places pour évitez les messages redondants
    - le lifeTime de typeorm est mis en place

--- ProjetFormation/Backend/src/shared/dto/categories/categories.dto.ts
+++ ProjetFormation/Backend/src/shared/dto/categories/categories.dto.ts
@@ -1,8 +1,19 @@
 import {IsNotBlank} from "../../../decorators/is-not-blank.decorator";
+import {IsString, MaxLength, MinLength} from "class-validator";
+import {ErrorGeneral, ErrorTypeCategories} from "../../utilities/error.enum";
 
 export class CategoriesDto {
-  //@IsString()
-  //@IsNotEmpty()
-  @IsNotBlank({message : 'Serveur : le nom ne peut pas être vide'})
-  nom?: string;
+  @IsNotBlank(
+      {message : 'DTO : ' + ErrorGeneral.EMPTY_ERROR}
+  )
+  @IsString(
+      {message : 'DTO : '+ ErrorTypeCategories.NOM_ERROR}
+  )
+  @MinLength(2,
+      {message : 'DTO : '+ ErrorTypeCategories.NOM_LENGTH}
+  )
+  @MaxLength(100,
+      {message : 'DTO : '+ ErrorTypeCategories.NOM_LENGTH}
+  )
+  nom: string;
 }
---
+++ ProjetFormation/Backend/src/shared/utilities/error.enum.ts
@@ -0,0 +1,18 @@
+export enum ErrorTypeCategories{
+    NOM_ERROR = "Le nom doit être une chaine de caractère",
+    NOM_LENGTH = "Le nom doit être compris entre 2 et 100 caractères",
+    DUPLICATE_USER = "user not created, duplicate entry",
+    ERROR_UNKNOW ="erreur inconnue du system"
+}
+
+export enum ErrorGeneral{
+    EMPTY_ERROR = "La valeur ne peut pas être vide",
+    USER_ALREADY_EXIST = "User already exist",
+    DUPLICATE_USER = "user not created, duplicate entry",
+    ERROR_UNKNOW ="erreur inconnue du system"
+}
+
+
+
+
+
