e4edac11b86f8df2720a1f32403259ba37357333
Snake7345 <thebasketman7@hotmail.com>
2023-04-04 16:37:28 +0200

    V0.8.16.6
    - Mise a jour du component Syllabus

--- ProjetFormation/Backend/src/syllabus/syllabus.service.ts
+++ ProjetFormation/Backend/src/syllabus/syllabus.service.ts
@@ -2,12 +2,33 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { SyllabusEntity } from "../shared/entities/syllabus.entity";
+import { RolesEntity } from "../shared/entities/roles.entity";
+import { RolesService } from "../roles/roles.service";
+import { FormationsEntity } from "../shared/entities/formations.entity";
+import { FormationsService } from "../formations/formations.service";
+import { UtilisateursDto } from "../shared/dto/utilisateurs/utilisateurs.dto";
+import { SyllabusDto } from "../shared/dto/syllabus/syllabus.dto";
 
 @Injectable()
 export class SyllabusService {
     constructor(
         @InjectRepository(SyllabusEntity)
         private syllabusRepository: Repository<SyllabusEntity>,
-    ) {
+        @InjectRepository(FormationsEntity)
+        private rolesRepository: Repository<FormationsEntity>,
+        private readonly formationService : FormationsService
+    )
+    {}
+    async getAll(): Promise<SyllabusDto[]> {
+        return (await this.syllabusRepository.find(
+          {
+              relations : {formations : true},
+              select : {
+                  idSyllabus : true,
+                  nom:true,
+                  actif:true,
+                  chemin:true,
+              }
+          })).map(s => ({ ...s, formations: s.formations}))
     }
 }
