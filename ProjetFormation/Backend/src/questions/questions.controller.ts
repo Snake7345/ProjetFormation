d46634fa0c81a3d5859dcf9f782448f987455507
Snake7345 <thebasketman7@hotmail.com>
2023-06-12 22:57:38 +0200

    V0.8.22.2
    - Certaines méthodes dans le component questions sont opérationnelles

--- ProjetFormation/Backend/src/questions/questions.controller.ts
+++ ProjetFormation/Backend/src/questions/questions.controller.ts
@@ -1,10 +1,21 @@
-import { Controller } from "@nestjs/common";
+import {Controller, Get, Param, ParseIntPipe} from "@nestjs/common";
 import { QuestionsService } from "./questions.service";
 import { ApiTags } from "@nestjs/swagger";
+import {QuestionsDto} from "../shared/dto/questions/questions.dto";
 
 @ApiTags("Questions")
 @Controller('questions')
 export class QuestionsController {
     constructor(private readonly questionsService: QuestionsService) {
+    }
+
+    @Get()
+    async GetAll() : Promise<QuestionsDto[]> {
+        return await this.questionsService.getAll();
+    }
+
+    @Get('question/:id')
+    async GetAllByID(@Param('id', ParseIntPipe) id: number) : Promise<QuestionsDto[]> {
+        return await this.questionsService.getAllByID(id);
     }
 }
--- ProjetFormation/Backend/src/questions/questions.service.ts
+++ ProjetFormation/Backend/src/questions/questions.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { QuestionsEntity } from "../shared/entities/questions.entity";
+import {SyllabusDto} from "../shared/dto/syllabus/syllabus.dto";
+import {QuestionsDto} from "../shared/dto/questions/questions.dto";
+import {UtilisateursDto} from "../shared/dto/utilisateurs/utilisateurs.dto";
 
 @Injectable()
 export class QuestionsService {
@@ -9,5 +12,36 @@ export class QuestionsService {
         @InjectRepository(QuestionsEntity)
         private questionsRepository: Repository<QuestionsEntity>,
     ) {
+    }
+
+    async getAll(): Promise<QuestionsDto[]> {
+        return (await this.questionsRepository.find(
+            {
+                relations : {formations : true},
+                select : {
+                    idQuestions : true,
+                    question:true,
+                    cote:true,
+                    actif:true,
+                }
+            })).map(q => ({ ...q, formation: q.question}))
+    }
+
+    async getAllByID(id:number): Promise<QuestionsDto[]> {
+        return (await this.questionsRepository.find(
+            {
+                relations : {formations : true},
+                select : {
+                    idQuestions : true,
+                    question:true,
+                    cote:true,
+                    actif:true,
+                },
+                where: {
+                    formations: {
+                        idFormations: id,
+                    },
+                },
+            })).map(q => ({ ...q, formation: q.formations}))
     }
 }
